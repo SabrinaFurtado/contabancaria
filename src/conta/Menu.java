0c4e83ccca346d7c346142f82892ee20cd7453bd
Sabrina Furtado <sabrina.s.furtado@gmail.com>
2024-02-22 11:29:59 -0300

    quarto commit

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,22 +1,14 @@
 package conta;
 
-import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 
 import java.util.Scanner;
+import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-
-		// TESTE CLASSE CONTA
-		Conta cont1 = new Conta(1, 8745, 1, "Sabrina", 20000.0f);
-		cont1.visualizar();
-		cont1.sacar(1500.0f);
-		cont1.visualizar();
-		cont1.depositar(600f);
-		cont1.visualizar();
 
 		// TESTE CLASSE CONTA CORRENTE
 		ContaCorrente cc1 = new ContaCorrente(2, 9685, 1, "Nicolas", 6000.0f, 5000.0f);
@@ -38,17 +30,18 @@ public class Menu {
 		int opcao;
 
 		while (true) {
+			System.out.println(Cores.TEXT_BLUE_BOLD_BRIGHT);
 			System.out.println("********************************");
-			System.out.println("           BANCO SAH            ");
+			System.out.println("*           BANCO SAH          *");
 			System.out.println("********************************");
-			System.out.println("       1 - CRIAR CONTA          ");
-			System.out.println("       2- SAQUE                 ");
-			System.out.println("       3- DEPOSITO              ");
-			System.out.println("       4- TRANFERIRÊCIA         ");
-			System.out.println("       5 - ATUALIZAR DADOS      ");
-			System.out.println("       0 - SAIR                 ");
+			System.out.println("*       1 - CRIAR CONTA        *");
+			System.out.println("*       2- SAQUE               *");
+			System.out.println("*      3- DEPOSITO             *");
+			System.out.println("*       4- TRANFERIRÊCIA       *");
+			System.out.println("*       5 - ATUALIZAR DADOS    *");
+			System.out.println("*       0 - SAIR               *");
 			System.out.println("********************************");
-			System.out.println("      DIGITE A OPÇÃO DESEJADA   ");
+			System.out.println("*      DIGITE A OPÇÃO DESEJADA *");
 
 			opcao = leia.nextInt();
 			
